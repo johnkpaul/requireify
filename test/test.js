fb3e8196639cd99079db71a7dfe61255966890cf
Todd Kennedy <todd@selfassembled.org>
2015-09-25 11:46:13 -0700

    Update detective to latest version
    Detective 2.x uses an old version of Esprima for parsing the files, this
    unforunately does not support ES6 syntax (I specifically had problems
    with template strings). The latest version of detective uses acorn which
    does support template strings correctly.
    
    Also updated the test to work in a later version of browserify as the
    output from bundle and the place into which to pass the `debug`
    parameter have moved.
    
    (Also hi!)

--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ var convert = require('convert-source-map');
 var modulePath = __dirname + '/module.js';
 var exported = __dirname + '/compiled.js';
 
-var b = browserify();
+var b = browserify({ debug: true });
 
 b.transform(requireify);
 
@@ -39,44 +39,40 @@ b.add(modulePath)
 
 // test for sourcemaps
 b.add(modulePath)
- .bundle({ debug: true }, function(err, src){
+ .bundle(function(err, src){
 
-   var sourceMapComment = src.split('\n').slice(-2)[0];
+   var sourceMapComment = src.toString('utf8').split('\n').slice(-2)[0];
    var json = convert.fromComment(sourceMapComment);
 
    //expected was found by first checking by hand and then saving those mappings
    //fs.writeFileSync('./expected-sourcemap', JSON.stringify(json));
    var expected = JSON.parse(require('./expected-sourcemap')).mappings;
-
   assert.equal(json.sourcemap.mappings, expected);
  });
- 
-var b = browserify();
+
+var b = browserify({ debug: true });
 
 
 // test for coffescript with sourcemaps
 b.transform(coffeeify).transform(requireify)
  .add(__dirname+'/coffee/foo.coffee')
- .bundle({ debug: true }, function(err, src){
+ .bundle(function(err, src){
    fs.writeFileSync(__dirname+'/compiled-for-coffee-source-maps.js', src);
-
-   var sourceMapComment = src.split('\n').slice(-2)[0];
+   var sourceMapComment = src.toString('utf8').split('\n').slice(-2)[0];
    var json = convert.fromComment(sourceMapComment);
-   //fs.writeFileSync('./expected-coffeescript-sourcemap', JSON.stringify(json));
-
+   // fs.writeFileSync('./expected-coffeescript-sourcemap', JSON.stringify(json));
 
    //expected was found by first checking by hand and then saving those mappings
    var expected = JSON.parse(require('./expected-coffescript-sourcemap')).mappings;
-
    assert.equal(json.sourcemap.mappings, expected);
  });
 
-var b = browserify();
+var b = browserify({ debug: true });
 
 // test for last curly brace error in issue #4
 b.transform(requireify)
  .add(__dirname+'/regressions/last-char-curly-brace.js')
- .bundle({ debug: true }, function(err, src){
+ .bundle(function(err, src){
     if(err){
       throw err;
     }
